16277df7fe08775c438d209ed7ffe926f9906fd0
leafarcfs <141395847+leafarcfs@users.noreply.github.com>
2025-12-05 18:46:23 -0300

    Update ChartCard.jsx

--- components/ChartCard.jsx
+++ components/ChartCard.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useRef } from 'react';
-import { Chart, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Chart, PieController, ArcElement, Tooltip, Legend } from 'chart.js';
+
+// Registrando explicitamente o PieController
+Chart.register(PieController, ArcElement, Tooltip, Legend);
+
 
 Chart.register(ArcElement, Tooltip, Legend);
 
